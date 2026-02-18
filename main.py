f5956717768fc607e3096932e8cce83e5d2b8436
kamrynv <156940405+kamrynv@users.noreply.github.com>
2026-02-17 18:42:36 -0600

    Update main.py
    using software pwm instead

--- main.py
+++ main.py
@@ -15,30 +15,32 @@ SERVOS = [SERVO_RED, SERVO_YELLOW, SERVO_GREEN, SERVO_BLUE] #later add additiona
 # INITIALIZATION:
 
 #servo init
+# for pin in SERVOS:
+#     GPIO.setup(pin, GPIO.OUT)
+
+#software pmw
+pwm = {}
+
 for pin in SERVOS:
     GPIO.setup(pin, GPIO.OUT)
+    pwm_i = GPIO.PWM(pin, 50)
+    pwm_i.start(0)
+    pwm[pin] = pwm_i
 
 
+def setAngle(angle, pin):
+    # frequency = 50 # 50 Hz for standard servos
+    # high_time = (angle / 180) * .002 + .0005
+    # GPIO.output(pin, GPIO.HIGH)
+    # time.sleep(high_time)
+    # GPIO.output(pin, GPIO.LOW)
+    # time.sleep(1/frequency - high_time)
 
-# def setAngle(angle, pin):
-#     frequency = 50 # 50 Hz for standard servos
-#     high_time = (angle / 180) * .002 + .0005
-#     GPIO.output(pin, GPIO.HIGH)
-#     time.sleep(high_time)
-#     GPIO.output(pin, GPIO.LOW)
-#     time.sleep(1/frequency - high_time)
+    duty = 2.5 + (angle / 180.0) * 10
+    pwm[pin].ChangeDutyCycle(duty)
+    time.sleep(0.5)
+    pwm[pin].ChangeDutyCycle(0)
 
-def setAngle(angle, pin, duration=0.5):
-    frequency = 50
-    period = 1 / frequency
-    high_time = (angle / 180) * 0.002 + 0.0005
-
-    end = time.time() + duration
-    while time.time() < end:
-        GPIO.output(pin, GPIO.HIGH)
-        time.sleep(high_time)
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(period - high_time)
 
 def move_open(pin):
     setAngle(0, pin)
@@ -55,5 +57,10 @@ try:
         time.sleep(2)
 
 except KeyboardInterrupt:
+    print('key board interupt')
+
+finally:
+    for i in pwm:
+        i.stop()
     GPIO.cleanup()
 
