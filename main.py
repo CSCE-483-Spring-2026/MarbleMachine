ca25b2236f2f213f034ae85168d0b9e241ed0807
kamrynv <156940405+kamrynv@users.noreply.github.com>
2026-02-24 18:51:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import yaml
 import datetime
+import threading
 
 GPIO.setmode(GPIO.BCM)
 
@@ -45,6 +46,13 @@ def move_closed(pin):
     #90 might be closed position for 3d print setup
     setAngle(90, pin)
 
+def moving_servos(pin, delay):
+    while True:
+        move_open(pin)
+        time.sleep(0.5) #need to test this value
+        move_closed(pin)
+        time.sleep(delay - 0.5*3) 
+
 def readYaml(filename):
     with open(filename, 'r') as file:
         return yaml.safe_load(file)
@@ -59,37 +67,57 @@ def interpretYaml(str):
     return li
     #would have format [['{marbleColor:', '{'Start':', 'startTime,', ''Period':', 'PeriodSet'][...][...][...]]
 
+#this is temporary, we can change to user input/ reading from the json eventually or whatever
+servo_delays = [2, 5, 10, 24]
+
+threads = []
+
+for i in range(4):
+    thread_i = threading.Thread(target=moving_servos, args=(SERVOS[i], servo_delays[i]))
+    thread_i.daemon = True #this is important apparently, kills threads because they'll never end
+    threads.append(thread_i)
+
+for i in range(4):
+    threads[i].start()
+
+
+
 #testing
 try:
+    while True:
+        time.sleep(1) 
 
-    move_open(SERVO_RED)
-    move_open(SERVO_YELLOW)
-    move_open(SERVO_GREEN)
-    move_open(SERVO_BLUE)
-    time.sleep(2)
-    move_closed(SERVO_RED)
-    time.sleep(2)
+    # move_open(SERVO_RED)
+    # move_open(SERVO_YELLOW)
+    # move_open(SERVO_GREEN)
+    # move_open(SERVO_BLUE)
+    # time.sleep(2)
+    # move_closed(SERVO_RED)
+    # time.sleep(2)
 
-    #move_open(SERVO_YELLOW)
-    time.sleep(2)
-    move_closed(SERVO_YELLOW)
-    time.sleep(2)
+    # #move_open(SERVO_YELLOW)
+    # time.sleep(2)
+    # move_closed(SERVO_YELLOW)
+    # time.sleep(2)
 
-    #move_open(SERVO_GREEN)
-    time.sleep(2)
-    move_closed(SERVO_GREEN)
-    time.sleep(2)
+    # #move_open(SERVO_GREEN)
+    # time.sleep(2)
+    # move_closed(SERVO_GREEN)
+    # time.sleep(2)
 
-    #move_open(SERVO_BLUE)
-    time.sleep(2)
-    move_closed(SERVO_BLUE)
-    time.sleep(2)
+    # #move_open(SERVO_BLUE)
+    # time.sleep(2)
+    # move_closed(SERVO_BLUE)
+    # time.sleep(2)
 
         
 
 
 except KeyboardInterrupt:
     print('key board interupt')
+    for i in pwm.values(): 
+        i.stop()
+    GPIO.cleanup()
 
 #i dont think this is working ?
 for i in pwm.values(): 
