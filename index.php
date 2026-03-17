28d69d3073c8a626b684102f4d7e45e4f6a2d6b0
lilyrosebooks <lilythomas@tamu.edu>
2026-03-17 09:28:04 -0500

    update index to php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
+
+<?php
+
+echo 'php file'
+
+$myObj = new marbleClass();
+$myObj->redFreq = \$_GET['redFreq'];
+$myObj->yellowFreq = \$_GET['yellowFreq'];
+$myObj->greenFreq = \$_GET['greenFreq'];
+$myObj->blueFreq = \$_GET['blueFreq'];
+
+$myJSON = json_encode($myObj);
+echo $myJSON;
+file_put_contents('test.json', $myJSON);
+
+?>
+
 <html>
 <head>
     <link rel="stylesheet" href="styles.css">
