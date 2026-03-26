45483b7ce7011c6c41675f76179d5877aea4cd40
lilyrosebooks <lilythomas@tamu.edu>
2026-03-26 12:23:00 -0500

    updated connect, need to update html

--- connect.php
+++ connect.php
@@ -4,9 +4,17 @@ echo 'php file';
 
 $myObj = array(
     'redFreq' => $_POST['redFreq'],
+    'redStartDay' => $_POST['redStartDay'],
+    'redStartTime' => $_POST['redStartTime'],
     'yellowFreq' => $_POST['yellowFreq'],
+    'yellowStartDay' => $_POST['yellowStartDay'],
+    'yellowStartTime' => $_POST['yellowStartTime'],
     'greenFreq' => $_POST['greenFreq'],
+    'greenStartDay' => $_POST['greenStartDay'],
+    'greenStartTime' => $_POST['greenStartTime'],
     'blueFreq' => $_POST['blueFreq']
+    'blueStartDay' => $_POST['blueStartDay'],
+    'blueStartTime' => $_POST['blueStartTime'],
 );
 
 $myJSON = json_encode($myObj);
