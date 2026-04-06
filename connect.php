1bf52db84ceeadb803609b37860da14d4f3ef70e
TrentonGray <trentonjgray@gmail.com>
2026-04-06 10:03:54 -0600

    Improve connect.php format and usability

--- connect.php
+++ connect.php
@@ -1,19 +1,33 @@
+<html>
+
 <?php
 
-echo 'php file';
-
+// Display jsonfile
+$jsonName = "test.json";
+echo "<h1>$jsonName file contents</h1>";
 $myObj = array(
     'redFreq' => $_POST['redFreq'],
     'yellowFreq' => $_POST['yellowFreq'],
     'greenFreq' => $_POST['greenFreq'],
     'blueFreq' => $_POST['blueFreq']
 );
+$json = json_encode($myObj);
+echo "<p>$json</p><hr>";
 
-$myJSON = json_encode($myObj);
-echo $myJSON;
-$tester = file_put_contents('test.json', $myJSON);
-echo $tester ? 'true' : 'false';
-$killer = shell_exec("pkill -f main.py");
-$pyOutput = shell_exec("/var/www/html/MarbleMachine/venv/bin/python /var/www/html/MarbleMachine/main.py >> /var/www/html/MarbleMachine/logs/mylog.txt 2>&1 < /dev/null &");
-echo "Started main.py...";
+echo "<h1>Debug Info</h1>";
+// Write json to folder
+$status = file_put_contents($jsonName, $json);
+echo $status ? "<p>Successfully made $jsonName</p>" : "<p>ERROR: Could not make $jsonName</p>";
+
+// Kill old main.py
+$status = shell_exec("pkill -f main.py");
+echo $status ? "ERROR: Could not kill main.py...\n" : "";
+
+// Start new main.py located in /var/www/html...
+$status = shell_exec("/var/www/html/MarbleMachine/venv/bin/python /var/www/html/MarbleMachine/main.py >> /var/www/html/MarbleMachine/logs/mylog.txt 2>&1 < /dev/null &");
+echo $status ? "ERROR: Error in starting main.py\n" : "";
+
+echo "<hr><p>Started main.py...</p>";
 ?>
+
+</html>
