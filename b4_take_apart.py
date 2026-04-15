16c5d6a6ab4a47c76a7a8c0b75df9c6427838527
kamrynv <156940405+kamrynv@users.noreply.github.com>
2026-04-15 10:33:47 -0500

    Update b4_take_apart.py

--- b4_take_apart.py
+++ b4_take_apart.py
@@ -86,11 +86,11 @@ move_all_the_way(SERVO_YELLOW)
 move_all_the_way(SERVO_GREEN)
 move_all_the_way(SERVO_BLUE)
 
-move_sort(0)
+#move_sort(0)
 # time.sleep(5)
 # move_sort(42)
 #move_sort(138)
-#move_sort(180)
+move_sort(180)
 
 
 
