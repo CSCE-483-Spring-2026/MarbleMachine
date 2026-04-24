4a839123996f05f79508bfee55906f9eb9835e32
kamrynv <156940405+kamrynv@users.noreply.github.com>
2026-04-23 22:21:45 -0500

    added close_all script

--- close_all.py
+++ close_all.py
@@ -81,22 +81,22 @@ def move_sort(angle):
 # move_open(SERVO_GREEN)
 # move_open(SERVO_BLUE)
 
-# move_closed(SERVO_RED)
-# move_closed(SERVO_YELLOW)
-# move_closed(SERVO_GREEN)
-# move_closed(SERVO_BLUE)
+move_closed(SERVO_RED)
+move_closed(SERVO_YELLOW)
+move_closed(SERVO_GREEN)
+move_closed(SERVO_BLUE)
 
 
-move_all_the_way(SERVO_RED)
-move_all_the_way(SERVO_YELLOW)
-move_all_the_way(SERVO_GREEN)
-move_all_the_way(SERVO_BLUE)
+# move_all_the_way(SERVO_RED)
+# move_all_the_way(SERVO_YELLOW)
+# move_all_the_way(SERVO_GREEN)
+# move_all_the_way(SERVO_BLUE)
 
 #move_sort(0)
 # time.sleep(5)
 # move_sort(42)
 #move_sort(138)
-move_sort(180)
+#move_sort(180)
 
 
 
